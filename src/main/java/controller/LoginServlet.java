53fb1d76e19e0fa198e4e7262c7f304c05733d8b
sandy150431 <sandy150431@gmail.com>
2019-03-29 18:42:44 +0800

    modify link Path

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -2,66 +2,56 @@ package controller;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-import model.Emp;
-import model.Workhours;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import dao.LoginDAO;
 import dao.NWorktimeDAO;
+import model.Emp;
+import model.Workhours;
 
-/**
- * Servlet implementation class LoginServlet
- */
-public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+@Controller
+public class LoginServlet {
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public LoginServlet() {
-		super();
-		// TODO Auto-generated constructor stub
+	@RequestMapping(path = "/main")
+	protected String main() {
+		return "login";
+	}
+	
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	protected void doGet(//
+			@RequestParam(value = "userNo", required = true) String userNo, //
+			@RequestParam(value = "password", required = true) String password) {
+		doPost(userNo, password);
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doPost(req, res);
-	}
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	protected String doPost(//
+			@RequestParam(value = "userNo", required = true) String userNo, //
+			@RequestParam(value = "password", required = true) String password) {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		req.setCharacterEncoding("UTF-8");
-		res.setContentType("text/html ; charset=UTF-8");
-
-		String userNo = req.getParameter("userNo");
-		String passWord = req.getParameter("password");
 		Emp emp = new Emp();
 		emp.setEmpNo(userNo);
-		emp.setPw(passWord);
+		emp.setPw(password);
 		LoginDAO logindao = new LoginDAO();
 		try {
 			String userValidate = logindao.authenticateUser(emp);
 			if (userValidate.equals("Admin")) {
 
-				req.getSession().setAttribute("userNo", userNo);
-				req.getRequestDispatcher("/WEB-INF/views/adminMain.jsp").forward(req, res);
+				// req.getSession().setAttribute("userNo", userNo);
+				// req.getRequestDispatcher("/WEB-INF/views/adminMain.jsp").forward(req, res);
+				return "admin/adminMain";
+
 			} else if (userValidate.equals("Manager")) {
-				
-				req.getSession().setAttribute("userNo", userNo);
-				req.getRequestDispatcher("/WEB-INF/views/managerMain.jsp").forward(req, res);
+
+				// req.getSession().setAttribute("userNo", userNo);
+				// req.getRequestDispatcher("/WEB-INF/views/managerMain.jsp").forward(req, res);
+				return "manager/managerMain";
 
 			} else if (userValidate.equals("Employee")) {
 				String yn = "";
@@ -70,24 +60,29 @@ public class LoginServlet extends HttpServlet {
 				NWorktimeDAO nworktimedao = new NWorktimeDAO();
 				yn = nworktimedao.findme(workhour);
 				if (yn.equals("oops")) {
-					req.getSession().setAttribute("hurry", "盡快繳交當周工時 ! !");
+					// req.getSession().setAttribute("hurry", "盡快繳交當周工時 ! !");
 				}
-				req.getSession().setAttribute("userNo", userNo);
-				req.getRequestDispatcher("/WEB-INF/views/employeeMain.jsp").forward(req, res);
+				// req.getSession().setAttribute("userNo", userNo);
+				// req.getRequestDispatcher("/WEB-INF/views/employeeMain.jsp").forward(req,
+				// res);
+				return "employee/employeeMain";
 
 			} else {
 				JFrame jf = new JFrame();
 				jf.setAlwaysOnTop(true);
 				JOptionPane.showMessageDialog(jf, "帳號或密碼錯誤");
 				jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				req.getSession().setAttribute("userNo", "NotUser");
-				req.getRequestDispatcher("/WEB-INF/views/Login.jsp").forward(req, res);
+				// req.getSession().setAttribute("userNo", "NotUser");
+				// req.getRequestDispatcher("/WEB-INF/views/Login.jsp").forward(req, res);
+				return "login";
 			}
-			
+
 		} catch (IOException ie) {
 			ie.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
+			return "login";
 		}
+		return "login";
 	}
 }
