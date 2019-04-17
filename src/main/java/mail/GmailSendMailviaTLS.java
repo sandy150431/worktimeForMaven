1a0db755c27df2c945a35b376a46908e600bf66e
sandy150431 <sandy150431@gmail.com>
2019-04-17 11:31:20 +0800

    先skip Email問題讓流程順利下去

--- src/main/java/mail/GmailSendMailviaTLS.java
+++ src/main/java/mail/GmailSendMailviaTLS.java
@@ -16,46 +16,45 @@ import dao.ForgotDAO;
 import model.Emp;
 
 public class GmailSendMailviaTLS {
- public static void main(String args[]) {
-	 ForgotDAO forgotDAO = new ForgotDAO();
-  String host = "smtp.gmail.com";
-  int port = 587;
-  final String username = "dingq84@gmail.com";
-  final String password = "ding5802";//your password
+	public static void main(String args[]) {
+		ForgotDAO forgotDAO = new ForgotDAO();
+		String host = "smtp.gmail.com";
+		int port = 587;
+		final String username = "dingq84@gmail.com";
+		final String password = "ding5802";// your password
 
-  Properties props = new Properties();
-  props.put("mail.smtp.host", host);
-  props.put("mail.smtp.auth", "true");
-  props.put("mail.smtp.starttls.enable", "true");
-  props.put("mail.smtp.port", port);
-  Session session = Session.getInstance(props, new Authenticator() {
-   protected PasswordAuthentication getPasswordAuthentication() {
-    return new PasswordAuthentication(username, password);
-   }
-  });
+		Properties props = new Properties();
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.port", port);
+		Session session = Session.getInstance(props, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
 
- try {
-//	  String s;
-//	  int i = (int)(Math.random() * 10000000 +1);
-//	   System.out.println(i);
-//	    s = String.valueOf(i);
-	 
-   Message message = new MimeMessage(session);
-   message.setFrom(new InternetAddress("dingq85@gmail.com"));
-   message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("dingq85@gmail.com"));
-   message.setSubject("忘記密碼.");
-   message.setText("Dear, \n\n 密碼已經變更為你的身分證!");
-   
-   Transport transport = session.getTransport("smtp");
-   transport.connect(host, port, username, password);
+		try {
+			// String s;
+			// int i = (int)(Math.random() * 10000000 +1);
+			// System.out.println(i);
+			// s = String.valueOf(i);
 
-   Transport.send(message);
-  
-   System.out.println("寄送email結束.");
-   
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("dingq85@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("dingq85@gmail.com"));
+			message.setSubject("忘記密碼.");
+			message.setText("Dear, \n\n 密碼已經變更為你的身分證!");
 
-  } catch (MessagingException e) {
-   throw new RuntimeException(e);
-  }
- }
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host, port, username, password);
+
+			Transport.send(message);
+
+			System.out.println("寄送email結束.");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- src/main/java/mail/NewEmpMail.java
+++ src/main/java/mail/NewEmpMail.java
@@ -1,5 +1,6 @@
 package mail;
 
+import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -10,50 +11,44 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.servlet.http.HttpSession;
-
 
 public class NewEmpMail {
- public static void main(String args[]) {
-	 
-  String host = "smtp.gmail.com";
-  int port = 587;
-  final String username = "dingq84@gmail.com";
-  final String password = "ding5802";//your password
 
-  Properties props = new Properties();
-  props.put("mail.smtp.host", host);
-  props.put("mail.smtp.auth", "true");
-  props.put("mail.smtp.starttls.enable", "true");
-  props.put("mail.smtp.port", port);
-  Session session = Session.getInstance(props, new Authenticator() {
-   protected PasswordAuthentication getPasswordAuthentication() {
-    return new PasswordAuthentication(username, password);
-   }
-  });
+	public static void sendMail(String recipient) {
 
- try {
-//	  String s;
-//	  int i = (int)(Math.random() * 10000000 +1);
-//	   System.out.println(i);
-//	    s = String.valueOf(i);
-	 
-   Message message = new MimeMessage(session);
-   message.setFrom(new InternetAddress("dingq85@gmail.com"));
-   message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("dingq85@gmail.com"));
-   message.setSubject("你的帳號已被建立");
-   message.setText("帳號為員工編號，密碼為你的身分證，請於登入後修改密碼");
-   
-   Transport transport = session.getTransport("smtp");
-   transport.connect(host, port, username, password);
+		String host = "localhost";
+		int port = 25;
+		final String username = "sandy150431@gmail.com";
 
-   Transport.send(message);
-  
-   System.out.println("寄送email結束.");
-   
+		Properties props = new Properties();
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", port);
+		//props.put("mail.smtp.auth", "true");
+		//props.put("mail.smtp.starttls.enable", "true");
+		Session session = Session.getInstance(props, null);
 
-  } catch (MessagingException e) {
-   throw new RuntimeException(e);
-  }
- }
+		try {
+			MimeMessage msg = new MimeMessage(session);
+		      //set message headers
+		      msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+		      msg.addHeader("format", "flowed");
+		      msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+		      msg.setFrom(new InternetAddress(username, "NoReply-JD"));
+		      //msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+		      msg.setSubject("你的帳號已被建立", "UTF-8");
+		      msg.setText("帳號為員工編號，密碼為你的身分證，請於登入後修改密碼", "UTF-8");
+		      msg.setSentDate(new Date());
+		      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient, false));
+	    	  Transport.send(msg);  
+
+		      System.out.println("EMail Sent Successfully!!");
+		      
+		} catch (MessagingException e) {
+			//throw new RuntimeException(e);
+		}catch (Exception e) {
+			//throw new RuntimeException(e);
+		}
+	}
+	
 }
